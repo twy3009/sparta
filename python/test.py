53a3bcb6168affc281ca13ec6f9582a194c6b5b2
twy3009 <61925189+twy3009@users.noreply.github.com>
2020-04-26 14:09:50 +0900

    test
    test

--- python/test.py
+++ python/test.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 from pymongo import MongoClient           # pymongo를 임포트 하기(패키지 인스톨 먼저 해야겠죠?)
-client = MongoClient('localhost', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
+client = MongoClient('mongod://test:test@15.164.103.10', 27017)  # mongoDB는 27017 포트로 돌아갑니다.
 db = client.dbsparta
 
 code_df = pd.read_html('http://kind.krx.co.kr/corpgeneral/corpList.do?method=download&searchType=13', header=0)[0]
